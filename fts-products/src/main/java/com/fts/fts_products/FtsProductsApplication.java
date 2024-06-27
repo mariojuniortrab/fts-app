4c2e6bfa0f18cb65b38d91560bb781aa2f4e0269
Mario Junior <mariojuniortrab@gmail.com>
2024-06-27 10:38:32 -0300

    fts-products created

---
+++ fts-products/src/main/java/com/fts/fts_products/FtsProductsApplication.java
@@ -0,0 +1,13 @@
+package com.fts.fts_products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtsProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtsProductsApplication.class, args);
+	}
+
+}
---
+++ fts-products/src/test/java/com/fts/fts_products/FtsProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fts.fts_products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtsProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
