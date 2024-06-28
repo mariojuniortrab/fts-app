affd26a3dd1758ef19f3655088ae0ae00d9f6dc9
Mario Junior <mariojuniortrab@gmail.com>
2024-06-27 22:32:30 -0300

    add project fts-user

---
+++ fts-user/src/test/java/com/fts/fts_user/FtsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fts.fts_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtsUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
