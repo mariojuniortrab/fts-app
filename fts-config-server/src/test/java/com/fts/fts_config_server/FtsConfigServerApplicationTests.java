3a3f19e4fe983d9e8ca82cdcc5567d5b93c4d19d
Mario Junior <mariojuniortrab@gmail.com>
2024-06-27 22:12:09 -0300

    configure server config

---
+++ fts-config-server/src/test/java/com/fts/fts_config_server/FtsConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fts.fts_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtsConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
