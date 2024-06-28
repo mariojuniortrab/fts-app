d138962badec8a76b127c948f9528f311d1c4b78
Mario Junior <mariojuniortrab@gmail.com>
2024-06-27 22:35:11 -0300

    configuring fts-user properties

--- fts-user/src/main/java/com/fts/fts_user/FtsUserApplication.java
+++ fts-user/src/main/java/com/fts/fts_user/FtsUserApplication.java
@@ -2,8 +2,10 @@ package com.fts.fts_user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FtsUserApplication {
 
 	public static void main(String[] args) {
